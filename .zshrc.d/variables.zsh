157b561d15050a2c5cedae494ca053611682c8d9
ykhi <ykhi@proton.me>
2025-09-29 18:22:27 +0200

    Add devbox and nix installers + variables.zsh

--- .zshrc.d/variables.zsh
+++ .zshrc.d/variables.zsh
@@ -4,6 +4,7 @@ export PATH="$PATH:$GOROOT"
 export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
 export PATH="$PATH:$GOBIN"
+export GOPATH="$HOME/go PATH=$PATH:/usr/local/go/bin:$GOPATH/bin"
 # pnpm
 export PNPM_HOME="/home/ykhi/.local/share/pnpm"
 export PATH="$PATH:$PNPM_HOME"
---
+++ installers/install_devbox.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+curl -fsSL https://get.jetify.com/devbox | bash
+
+
